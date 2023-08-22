99db763e830e774cfbf1b64c678ec2851ae4ccfc
dpkolee <deepakolee@gmail.com>
2023-08-22 23:11:59 +0545

    Chapter 6 : - Express

--- server.js
+++ server.js
@@ -1,105 +1,33 @@
-const fs = require("fs");
-const fsPromises = require("fs").promises;
 const path = require("path");
-const http = require("http");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
-const logEvents = require("./logEvents");
-
-const EventEmitter = require("events");
-
-class Emitter extends EventEmitter {}
-
-const myEmitter = new Emitter();
-
-myEmitter.on("log", (msg, fileName) => logEvents(msg, fileName));
-
-const PORT = process.env.PORT || 3500;
-
-const serveFile = async (filePath, contentType, response) => {
-  try {
-    const rawData = await fsPromises.readFile(
-      filePath,
-      !contentType.includes("image") ? "utf8" : ""
-    );
-    const data =
-      contentType === "application/json" ? JSON.parse(rawData) : rawData;
-    response.writeHead(200, { "COntent-Type": contentType });
-    response.end(
-      contentType === "application/json" ? JSON.stringify(data) : data
-    );
-  } catch (error) {
-    myEmitter.emit("log", `${error.name}: ${error.message}`, "errorLog.txt");
-    response.status = 500;
-    response.end();
-  }
+const one = (req, res, next) => {
+  console.log("one");
+  next();
+};
+const two = (req, res, next) => {
+  console.log("two");
+  next();
+};
+const three = (req, res) => {
+  console.log("three");
+  res.send("Finished");
 };
 
-const server = http.createServer((req, res) => {
-  myEmitter.emit("log", `${req.url}\t${req.method}`, "reqLog.txt");
-  const fileExtension = path.extname(req.url);
-
-  let contentType;
-  switch (fileExtension) {
-    case ".js":
-      contentType = "text/javascript";
-      break;
-    case ".css":
-      contentType = "text/css";
-      break;
-    case ".txt":
-      contentType = "text/plain";
-      break;
-    case ".json":
-      contentType = "application/json";
-      break;
-    case ".jpg":
-      contentType = "image/jpeg";
-      break;
-    case ".png":
-      contentType = "image/png";
-      break;
-    default:
-      contentType = "text/html";
-      break;
-  }
-
-  let filePath =
-    contentType === "text/html" && req.url === "/"
-      ? path.join(__dirname, "views", "index.html")
-      : contentType === "text/html" && req.url.slice(-1) === "/"
-      ? path.join(__dirname, "views", req.url, "index.html")
-      : contentType === "text/html"
-      ? path.join(__dirname, "views", req.url)
-      : path.join(__dirname, req.url);
-
-  //makes .html extension not required in the browser
-  if (!fileExtension && req.url.slice(-1) !== "/") filePath += ".html";
-
-  const fileExists = fs.existsSync(filePath);
-  console.log("fileExists", fileExists);
-
-  if (fileExists) {
-    serveFile(filePath, contentType, res);
-  } else {
-    switch (path.parse(filePath).base) {
-      case "old-page.html":
-        res.writeHead(301, {
-          Location: "/new-page.html",
-        });
-        res.end();
-        break;
-
-      case "www-page.html":
-        res.writeHead(301, {
-          Location: "/",
-        });
-        res.end();
-        break;
-
-      default:
-        serveFile(path.join(__dirname, "views", "404.html"), "text/html", res);
-        break;
-    }
-  }
+app.get("^/$|index(.html)?", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "index.html"))
+);
+app.get("/new-page(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
 });
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "/new-page");
+});
+app.get("/chain(.html)?", [one, two, three]);
+app.get("/*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
