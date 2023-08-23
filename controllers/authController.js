5848521467f5ff43c3534bd33bc795d69f48db1e
dpkolee <deepakolee@gmail.com>
2023-08-23 17:59:13 +0545

    Chapter 10: - Authentication

---
+++ controllers/authController.js
@@ -0,0 +1,38 @@
+const userDB = {
+  users: require("../model/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+
+const bcrypt = require("bcrypt");
+
+const handleLogin = async (req, res) => {
+  const { user, pwd } = req.body;
+  if (!user | !pwd)
+    return res
+      .status(400)
+      .json({ message: "Username and Password are required" });
+
+  const foundUser = userDB.users.find((dbUser) => dbUser.username === user);
+
+  if (!foundUser)
+    return res.status(401).json({
+      message: "User not registered",
+    });
+
+  const match = await bcrypt.compare(pwd, foundUser.password);
+
+  if (match) {
+    //create JWT's
+    res.json({
+      message: "Successfully loggedIn User",
+    });
+  } else {
+    res.status(401).json({
+      message: "Unauthorized User!!",
+    });
+  }
+};
+
+module.exports = { handleLogin };
---
+++ controllers/registerController.js
@@ -0,0 +1,51 @@
+const userDB = {
+  users: require("../model/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+
+const fsPromises = require("fs").promises;
+const path = require("path");
+const bcrypt = require("bcrypt");
+
+const handleNewUser = async (req, res) => {
+  const { user, pwd } = req.body;
+  if (!user | !pwd)
+    return res
+      .status(400)
+      .json({ message: "Username and Password are required" });
+
+  //Check for duplicate username in database
+  const duplicateUser = userDB.users.find((dbUser) => dbUser.username === user);
+  if (duplicateUser)
+    return res
+      .status(409)
+      .json({ message: "User has been already created" });
+
+  try {
+    //encrypt the password
+    const hashedPwd = await bcrypt.hash(pwd, 10);
+
+    //store the new user
+    const newUser = {
+      username: user,
+      password: hashedPwd,
+    };
+    userDB.setUsers([...userDB.users, newUser]);
+    await fsPromises.writeFile(
+      path.join(__dirname, "..", "model", "users.json"),
+      JSON.stringify(userDB.users)
+    );
+    console.log("userDB.users", userDB.users);
+    res.status(201).json({
+      message: "New User has been created",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  handleNewUser,
+};
