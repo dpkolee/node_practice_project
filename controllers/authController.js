bf75ea87b3e5b9b9d39855c1d5e995477a04a732
dpkolee <deepakolee@gmail.com>
2023-08-23 22:57:00 +0545

    Chapter 10: - Authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,7 +9,7 @@ const bcrypt = require("bcrypt");
 
 const handleLogin = async (req, res) => {
   const { user, pwd } = req.body;
-  if (!user | !pwd)
+  if (!user || !pwd)
     return res
       .status(400)
       .json({ message: "Username and Password are required" });
