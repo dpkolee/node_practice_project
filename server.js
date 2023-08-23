c9ccf67a768b5338b57075f5fda38ae89eca7bca
dpkolee <deepakolee@gmail.com>
2023-08-23 14:31:00 +0545

    Chapter 8: - Routing

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const errorHandler = require("./middleware/errorHandler");
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const router = require("./routes/subdir");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -33,34 +34,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // serve static files
-app.use(express.static(path.join(__dirname, "/public")));
+app.use("/", express.static(path.join(__dirname, "/public")));
+app.use("/subdir", express.static(path.join(__dirname, "/public")));
 
-const one = (req, res, next) => {
-  console.log("one");
-  next();
-};
-const two = (req, res, next) => {
-  console.log("two");
-  next();
-};
-const three = (req, res) => {
-  console.log("three");
-  res.send("Finished");
-};
+app.use("/", require("./routes/root"));
+app.use("/employees", require("./routes/api/employees"));
+app.use("/subdir", router);
 
-app.get("^/$|index(.html)?", (req, res) =>
-  res.sendFile(path.join(__dirname, "views", "index.html"))
-);
+// const one = (req, res, next) => {
+//   console.log("one");
+//   next();
+// };
+// const two = (req, res, next) => {
+//   console.log("two");
+//   next();
+// };
+// const three = (req, res) => {
+//   console.log("three");
+//   res.send("Finished");
+// };
 
-app.get("/new-page(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "new-page.html"));
-});
-
-app.get("/old-page(.html)?", (req, res) => {
-  res.redirect(301, "/new-page");
-});
-
-app.get("/chain(.html)?", [one, two, three]);
+// app.get("/chain(.html)?", [one, two, three]);
 
 app.all("*", (req, res) => {
   res.status(404);
